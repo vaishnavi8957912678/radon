84186cebabec1fcd075269736abce673fbc70766
Aishwarya Sejgaya <aishwaryasejgaya0240@gmail.com>
2022-06-29 13:08:40 +0530

    done

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -18,10 +18,10 @@ const college = async function (req,res){
 
 const getColleges = async function (req,res){
     try{
-    let college = req.query.collegeId;
+    let college = req.query.collegeName;
      
    let result= await collegeModel.findOne({_id : college}).select({name:1, fullName: 1, logoLink: 1, _id:0})
-   let interns = await internModel.find({collegeId : req.query.collegeId}).select({name:1, email: 1, mobile: 1})
+   let interns = await internModel.find({collegeId : req.query.collegeName}).select({name:1, email: 1, mobile: 1})
    
     return res.status(200).send({status:true, Data: result,interns});
     } catch (err){
